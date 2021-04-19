42a4257323e0fccf478644e57ce462552c5d79f7
Rohan-Prabhakar <76002659+Rohan-Prabhakar@users.noreply.github.com>
2021-04-19 18:56:21 +0530

    Update DeleteServlet.java

--- src/java/DeleteServlet.java
+++ src/java/DeleteServlet.java
@@ -1,5 +1,6 @@
 import java.io.IOException;
 import java.sql.Connection;
+import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
